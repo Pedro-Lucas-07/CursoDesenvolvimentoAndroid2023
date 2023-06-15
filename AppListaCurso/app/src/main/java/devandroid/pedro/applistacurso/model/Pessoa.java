7a387b03f56280c0eff739089160cfa18b675fc8
Pedro-Lucas-07 <fastest077@gmail.com>
2023-06-15 09:52:38 -0300

    Uso dos métodos Get e Set.

--- AppListaCurso/app/src/main/java/devandroid/pedro/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/pedro/applistacurso/model/Pessoa.java
@@ -2,6 +2,45 @@ package devandroid.pedro.applistacurso.model;
 
 public class Pessoa {
     //Construtor
+    //Atributos
+    //Métodos de Acesso - Getters and Setters
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
 
     public Pessoa(){
 
--- AppListaCurso/app/src/main/java/devandroid/pedro/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/pedro/applistacurso/view/MainActivity.java
@@ -10,12 +10,49 @@ import devandroid.pedro.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        outraPessoa = new Pessoa();
+
+        pessoa.setPrimeiroNome("Pedro");
+        pessoa.setSobreNome("Lucas");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("1140028922");
+
+        dadosPessoa = "Primeiro Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+
+        outraPessoa.setPrimeiroNome("Yasmin");
+        outraPessoa.setSobreNome("Caroline");
+        outraPessoa.setCursoDesejado("Moda");
+        outraPessoa.setTelefoneContato("1199477654");
+
+        dadosOutraPessoa = "Primeiro Nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+        int parada = 0;
 
     }
 }
